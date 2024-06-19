07d457db43ebdce46bf58f14871a9f59d8680cb8
adipfstk <placinta.adrianstefan@gmail.com>
2024-06-19 19:47:44 +0300

    It seems it works

--- sorting-algorithms/src/Main.java
+++ sorting-algorithms/src/Main.java
@@ -4,38 +4,23 @@
 
 public class Main {
 
-    public static void quickSort(int[] arr, int start, int end) {
-        if (start>=end) {
-            return;
-        }
 
-        int indexPivot =  partition(arr, start, end);
-        quickSort(arr, start, indexPivot -1);
-        quickSort(arr, indexPivot + 1, end);
-    }
-    public static int partition(int[] arr, int start, int end) {
-        int pivot = arr[end];
-        int i = start - 1;
-
-        for (; start < end; start++) {
-            if (arr[start] <= pivot) {
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[start];
-                arr[start] = temp;
+    public static void insertionSort(int[] arr) {
+        int j;
+        for (int i = 1; i < arr.length;i++) {
+            j = i - 1;
+            int temp = arr[i];
+            while (j>=0 && temp >= arr[j]) {
+                arr[j+1] = arr[j];
+                j--;
             }
+            arr[j+1] = temp;
         }
-
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[end];
-        arr[end] = temp;
-
-        return i + 1;
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[] {5,4,3,2,1};
-        quickSort(arr, 0, arr.length-1);
+        int[] arr = new int[] {9,1,3,7};
+        insertionSort(arr);
         for (int x: arr) {
             System.out.println(x);
         }
